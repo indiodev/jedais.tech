d31362ee615a6409e6d18711d85a69e6a574953f
jhollyfer <jhollyfer.fr@gmail.com>
2024-02-20 22:12:24 -0500

    feat: configure initial page

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,28 @@ import type { ReactElement } from 'react';
 
 export function App(): ReactElement {
 	return (
-		<>
-			<h1>Hello Jedais</h1>
-		</>
+		<div className="h-screen overflow-hidden bg-primary">
+			<header className="flex items-center justify-center w-full">
+				<nav className="container flex items-center justify-between py-4">
+					<img src="/default-logo.png" />
+
+					<ul className="flex gap-4 text-secondary">
+						<li>
+							<a href="#">Inicio</a>
+						</li>
+						<li>
+							<a href="#">Blog</a>
+						</li>
+						<li>
+							<a href="#">Link 3</a>
+						</li>
+					</ul>
+				</nav>
+			</header>
+
+			<section className="flex flex-1 h-full border items-center justify-center">
+				<h1 className="text-secondary">Em construção...</h1>
+			</section>
+		</div>
 	);
 }
